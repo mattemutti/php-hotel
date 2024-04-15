f04a8a102806afff7f732f255d82c7ed4846cb0f
mattemutti <mattemutti@gmail.com>
2024-04-15 17:30:17 +0200

    update data card

--- index.php
+++ index.php
@@ -56,6 +56,7 @@
 	<meta charset="UTF-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.2/css/all.min.css" integrity="sha512-SnH5WK+bZxgPHs44uWIX+LLJAJ9/2PkPKZ5QiAj6Ta86w+fsb2TkcmfRyVX3pBnMFcV7oQPJkl9QevSCWr3W6A==" crossorigin="anonymous" referrerpolicy="no-referrer" />
 	<title>PhP_Hotel</title>
 </head>
 <style>
@@ -67,22 +68,28 @@
 </style>
 <body>
 
-<div class="container text-center p-4">
+<div class="container text-center p-4 text-uppercase">
 	<h1>Hotels</h1>
 </div>
 
 
 <div class="container">
 	<div class="row justify-content-center">
-		<?php foreach ($hotels as $hotelArray) : ?>
-			<div class="card col-4 p-4 gap-2" style="">
+		<?php foreach ($hotels as $key => $hotel) : ?>
+			<div class="card col-4 p-4" style="">
 				<img src="https://picsum.photos/200" class="card-img-top" alt="...">
 				<div class="card-body">
 				
-					<?php foreach ($hotelArray as $key => $hotel) : ?>	
-    					<h5 class="card-title"> <?= "$key: $hotel" ?> </h5>
-    					<p class="card-text"></p>
-					<?php endforeach; ?>
+					<h5 class="card-title"> <?= $hotel['name'] ?> </h5>
+					<p class="card-text"> <?= $hotel['description'] ?> </p>
+					<p class="card-text">Voto: <?= $hotel['vote'] ?> </p>
+					<p class="card-text">Distanza dal Centro: <?= $hotel['distance_to_center'] ?> km</p>
+
+					<?php if ($hotel['parking'] === true) : ?>
+						<p><i class="fa-solid fa-check"></i> Yes c'è il parcheggio</p>
+					<?php else : ?>
+						<p><i class="fa-solid fa-x"></i> Mi spiace, non c'è il parcheggio </p>
+				<?php endif; ?>
     		
 					<a href="#" class="btn btn-primary">Prenota</a>
 				</div>
